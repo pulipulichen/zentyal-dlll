2f20e61b1bcbaf27074efda1edd648722d235ea5
Pulipuli Chen <pulipuli.chen@gmail.com>
2013-08-06 22:01:39 +0800

    整合模組完成

--- pound/debs-ppa/zentyal-pound_3.0.dsc
+++ pound/debs-ppa/zentyal-pound_3.0.dsc
@@ -10,8 +10,8 @@ Build-Depends: debhelper (>= 7.0.0), cdbs
 Package-List: 
  zentyal-pound deb web optional
 Checksums-Sha1: 
- 52fd1f51dd103e9a40c4a31595968d69370fcc1e 618360 zentyal-pound_3.0.tar.gz
+ 0a089c02bf5ec42287c8648956e9cdab809f376f 2643590 zentyal-pound_3.0.tar.gz
 Checksums-Sha256: 
- 2e89b70cec27b90281bdf1a7be906abc34ac009e0fdece5f7a13090fb337934e 618360 zentyal-pound_3.0.tar.gz
+ 45a937242b2e9cd11c0b5fbb070ee8d83a8620080a37645629baa0abbc26b68b 2643590 zentyal-pound_3.0.tar.gz
 Files: 
- 519b8df5d37b5bfa43531f6b1a2a5f20 618360 zentyal-pound_3.0.tar.gz
+ c42783aba7760b971463fabe70dca960 2643590 zentyal-pound_3.0.tar.gz
--- pound/src/EBox/Pound/Model/PoundServices.pm
+++ pound/src/EBox/Pound/Model/PoundServices.pm
@@ -68,19 +68,70 @@ sub _table
             'fieldName' => 'redirHTTP',
             'printableName' => __('HTTP Redirect'),
             'editable' => 1,
-            'populate' => \&populateHTTP
-#            'populate' => (
-#                {
-#                    'value' => 'redirHTTP_default',
-#                    'printableValue' => __("Use Internal Port"),
-#                },
-#                {
-#                    'value' => 'redirHTTP_disable',
-#                    'printableValue' => __("Disable"),
-#                },
-#            ),
+            'populate' => \&populateHTTP,
+        ),
+        new EBox::Types::Union(
+            'fieldName' => 'redirHTTPS',
+            'printableName' => __('HTTPS Redirect'),
+            'subtypes' =>
+            [
+            new EBox::Types::Port(
+                'fieldName' => 'redirHTTPS_default',
+                'printableName' => __('Default HTTPS port (443)'),
+                'defaultValue' => 443,
+                'hidden' => 1,
+                'editable' => 0,),
+            new EBox::Types::Port(
+                'fieldName' => 'redirHTTPS_other',
+                'printableName' => __('Other'),
+                'editable' => 1,),
+            new EBox::Types::Union::Text(
+                'fieldName' => 'redirHTTPS_disable',
+                'printableName' => __('Disable')),
+            ]
+        ),
+        new EBox::Types::Union(
+            'fieldName' => 'redirSSH',
+            'printableName' => __('SSH Redirect'),
+            'subtypes' =>
+            [
+            new EBox::Types::Port(
+                'fieldName' => 'redirSSH_default',
+                'printableName' => __('Default SSH port (22)'),
+                'defaultValue' => 22,
+                'hidden' => 1,
+                'editable' => 0,),
+            new EBox::Types::Port(
+                'fieldName' => 'redirSSH_other',
+                'printableName' => __('Other'),
+                'editable' => 1,),
+            new EBox::Types::Union::Text(
+                'fieldName' => 'redirSSH_disable',
+                'printableName' => __('Disable')),
+            ]
+        ),
+        new EBox::Types::Union(
+            'fieldName' => 'redirRDP',
+            'printableName' => __('RDP Redirect'),
+            'subtypes' =>
+            [
+            new EBox::Types::Port(
+                'fieldName' => 'redirRDP_default',
+                'printableName' => __('Default RDP port (3389)'),
+                'defaultValue' => 3389,
+                'hidden' => 1,
+                'editable' => 0,),
+            new EBox::Types::Port(
+                'fieldName' => 'redirRDP_other',
+                'printableName' => __('Other'),
+                'editable' => 1,),
+            new EBox::Types::Union::Text(
+                'fieldName' => 'redirRDP_disable',
+                'printableName' => __('Disable')),
+            ]
         ),
 
+        # ==============================
         # Enable Keep Last
         new EBox::Types::Boolean(
             fieldName => 'enabled',
@@ -106,6 +157,115 @@ sub _table
 
     return $dataTable;
 }
+
+# ---------------------------------------
+
+sub addedRowNotify
+{
+    my ($self, $row) = @_;
+    $self->addDomainName($row);
+    $self->addRedirects($row);
+}
+
+sub deletedRowNotify
+{
+    my ($self, $row) = @_;
+    $self->deletedDomainName($row);
+    $self->deletedRedirects($row);
+}
+
+sub updatedRowNotify
+{
+    my ($self, $row, $oldRow) = @_;
+    $self->deletedRowNotify($oldRow);
+    $self->addedRowNotify($row);
+
+    $self->deletedRedirects($row);
+    $self->addRedirects($row);
+}
+
+# ---------------------------------------
+
+sub addDomainName
+{
+    my ($self, $row) = @_;
+
+    if ($row->valueByName('boundLocalDns') && $row->valueByName('enabled')) {
+        my $domainName = $row->valueByName('domainName');
+        my $gl = EBox::Global->getInstance();
+        my $dns = $gl->modInstance('dns');
+        $dns->addDomain({
+            domain_name => $domainName,
+        });
+    }
+}
+
+sub deletedDomainName
+{
+    my ($self, $row) = @_;
+    my $domainName = $row->valueByName('domainName');
+
+    my $gl = EBox::Global->getInstance();
+    my $dns = $gl->modInstance('dns');
+    my $domModel = $dns->model('DomainTable');
+    my $id = $domModel->findId(domain => $domainName);
+    if (defined($id)) 
+    {
+        $domModel->removeRow($id);
+    }
+}
+
+# -----------------------------
+
+sub addRedirects
+{
+    my ($self, $row) = @_;
+
+    if ($row->valueByName('enabled')) {
+        # 加入HTTP
+        if ($row->valueByName('redirHTTP') ne 'redirHTTP_disable') {
+            my %param = $self->getRedirectParamHTTP($row);
+            $self->addRedirectRow(%param);
+        }
+
+        # 加入HTTPS
+        if ($row->valueByName('redirHTTPS') ne 'redirHTTPS_disable') {
+            my %param = $self->getRedirectParamHTTPS($row);
+            $self->addRedirectRow(%param);
+        }
+        
+        # 加入SSH
+        if ($row->valueByName('redirSSH') ne 'redirSSH_disable') {
+            my %param = $self->getRedirectParamSSH($row);
+            $self->addRedirectRow(%param);
+        }
+        
+        # 加入RDP
+        if ($row->valueByName('redirRDP') ne 'redirRDP_disable') {
+            my %param = $self->getRedirectParamRDP($row);
+            $self->addRedirectRow(%param);
+        }
+    }
+}
+
+sub deletedRedirects
+{
+    my ($self, $row) = @_;
+
+    my %param = $self->getRedirectParamHTTP($row);
+    $self->deleteRedirectRow(%param);
+
+    %param = $self->getRedirectParamHTTPS($row);
+    $self->deleteRedirectRow(%param);
+    
+    %param = $self->getRedirectParamSSH($row);
+    $self->deleteRedirectRow(%param);
+
+    %param = $self->getRedirectParamRDP($row);
+    $self->deleteRedirectRow(%param);
+}
+
+# -----------------------------
 
 sub populateHTTP
 {
@@ -171,83 +331,75 @@ sub getPortHeader
      return $portHeader;
 }
 
-sub addedRowNotify
-{
-    my ($self, $row) = @_;
-    $self->addDomainName($row);
-    $self->addRedirects($row);
-}
 
-sub deletedRowNotify
-{
-    my ($self, $row) = @_;
-    $self->deletedDomainName($row);
-    $self->deletedRedirects($row);
-}
-
-sub updatedRowNotify
-{
-    my ($self, $row, $oldRow) = @_;
-    $self->deletedRowNotify($oldRow);
-    $self->addedRowNotify($row);
-}
-
-sub addDomainName
+sub getRedirectParamHTTP
 {
     my ($self, $row) = @_;
 
-    if ($row->valueByName('boundLocalDns') && $row->valueByName('enabled')) {
-        my $domainName = $row->valueByName('domainName');
-        my $gl = EBox::Global->getInstance();
-        my $dns = $gl->modInstance('dns');
-        $dns->addDomain({
-            domain_name => $domainName,
-            #ipAddresses => $self->getExternalIpaddr(),
-        });
+    my $portHeader = $self->getPortHeader($row);
+
+    my $extPort = $portHeader . '80';
+    my $intPort = $row->valueByName('port');
+
+    return $self->getRedirectParameter($row, $extPort, $intPort);
+}
+
+sub getRedirectParamHTTPS
+{
+    my ($self, $row) = @_;
+
+    my $portHeader = $self->getPortHeader($row);
+
+    my $extPort = $portHeader . '43';
+    my $intPort = 443;
+    if ($row->valueByName('redirHTTPS') eq 'redirHTTPS_other') 
+    {
+        $intPort = $row->valueByName('redirHTTPS_other');
     }
+
+    return $self->getRedirectParameter($row, $extPort, $intPort);
 }
 
-sub deletedDomainName
-{
-    my ($self, $row) = @_;
-    my $domainName = $row->valueByName('domainName');
-
-    my $gl = EBox::Global->getInstance();
-    my $dns = $gl->modInstance('dns');
-    my $domModel = $dns->model('DomainTable');
-    my $id = $domModel->findId(domain => $domainName);
-    $domModel->removeRow($id);
-}
-
-sub addRedirects
+sub getRedirectParamSSH
 {
     my ($self, $row) = @_;
 
-    if ($row->valueByName('enabled')) {
-        my $portHeader = $self->getPortHeader($row);
-        
-        my $gl = EBox::Global->getInstance();
-        my $firewall = $gl->modInstance('firewall');
-        my $redirMod = $firewall->model('RedirectsTable');
-        
-        # 加入HTTP
-        if ($row->valueByName('redirHTTP') ne 'redirHTTP_disable') {
-            my $extPort = $portHeader . '80';
-            my $intPort = $row->valueByName('port');
-            $self->addRedirectRow($row, $redirMod, $extPort, $intPort);
-        }
+    my $portHeader = $self->getPortHeader($row);
 
+    my $extPort = $portHeader . '22';
+    my $intPort = 22;
+    if ($row->valueByName('redirSSH') eq 'redirSSH_other') 
+    {
+        $intPort = $row->valueByName('redirSSH_other');
     }
+
+    return $self->getRedirectParameter($row, $extPort, $intPort);
 }
 
-sub addRedirectRow
+sub getRedirectParamRDP
 {
-    my ($self, $row, $redirMod, $extPort, $intPort) = @_;
-    
+    my ($self, $row) = @_;
+
+    my $portHeader = $self->getPortHeader($row);
+
+    my $extPort = $portHeader . '89';
+    my $intPort = 3389;
+    if ($row->valueByName('redirRDP') eq 'redirRDP_other') 
+    {
+        $intPort = $row->valueByName('redirRDP_other');
+    }
+
+    return $self->getRedirectParameter($row, $extPort, $intPort);
+}
+
+sub getRedirectParameter
+{
+    my ($self, $row, $extPort, $intPort) = @_;
+
     my $iface = $self->getExternalIface();
     my $localIpaddr = $row->valueByName('ipaddr');
 
-    $redirMod->addRow(
+    return (
         interface => $iface,
         origDest_selected => "origDest_ebox",
         protocol => "tcp/udp",
@@ -257,16 +409,35 @@ sub addRedirectRow
         destination => $localIpaddr,
         destination_port_selected => "destination_port_other",
         destination_port_other => $intPort,
+        description => 'Created by Pound Moudle',
         snat => 1,
         log => 0,
     );
 }
 
-sub deletedRedirects
+sub addRedirectRow
 {
-    my ($self, $row) = @_;
-
+    my ($self, %params) = @_;
     
+    my $gl = EBox::Global->getInstance();
+    my $firewall = $gl->modInstance('firewall');
+    my $redirMod = $firewall->model('RedirectsTable');
+
+    $redirMod->addRow(%params);
+}
+
+sub deleteRedirectRow
+{
+    my ($self, %param) = @_;
+    
+    my $gl = EBox::Global->getInstance();
+    my $firewall = $gl->modInstance('firewall');
+    my $redirMod = $firewall->model('RedirectsTable');
+
+    my $id = $redirMod->findId(%param);
+    if (defined($id)) {
+        $redirMod->removeRow($id);
+    }
 }
 
 # 找尋row用
