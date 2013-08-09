448567dd58f51affd2c27066e949d9433eb04a1d
Pulipuli Chen <pulipuli.chen@gmail.com>
2013-08-10 02:58:47 +0800

    大範圍修改前準備

--- pound/src/EBox/Pound/Model/Settings.pm
+++ pound/src/EBox/Pound/Model/Settings.pm
@@ -110,4 +110,139 @@ sub _table
     return $dataTable;
 }
 
+sub updatedRowNotify
+{
+    my ($self, $row, $oldRow) = @_;
+
+    
+}
+
+sub getServiceParam
+{
+    my ($self) = @_;
+
+    return (
+        # 寫入參數
+    );
+}
+
+sub checkService
+{
+    my ($self) = @_;
+
+    # 確認服務是否存在
+    my $existed = 0;
+    my $param = $self->getServiceParam();
+
+    # 載入網路模組
+    # 確認
+
+    return $existed;
+}
+
+sub addService
+{
+    my ($self) = @_;
+    
+    my $param = $self->getServiceParam();
+
+    # 新增服務
+    
+}
+
+sub getServiceRow
+{
+     my ($self) = @_;
+
+    my $row;
+    
+    my $param = $self->getServiceParam();
+
+    return $row
+}
+
+sub getServicePortParam
+{
+    my ($self, $port) = @_;
+
+    return (
+        # 設定連接埠參數
+    );
+}
+
+# ------------------
+
+sub checkServicePort
+{
+    my ($self, $row) = @_;
+
+    my $serviceRow = $self->getServiceRow();
+    my $port = $row->valueByName("port");
+    my $param = $self->getServicePortParam($port);
+}
+
+sub addServicePort
+{
+    my ($self, $row) = @_;
+
+    my $serviceRow = $self->getServiceRow();
+    my $port = $row->valueByName("port");
+    my $param = $self->getServicePortParam($port);
+}
+
+sub deleteServicePort
+{
+    my ($self, $row) = @_;
+
+    my $serviceRow = $self->getServiceRow();
+    my $port = $row->valueByName("port");
+    my $param = $self->getServicePortParam($port);
+}
+
+# ----------------------
+
+sub checkFilter
+{
+    my ($self) = @_;
+
+    # 確認服務是否存在
+    my $existed = 0;
+    my $param = $self->getFilterParam();
+
+    # 載入網路模組
+    # 確認
+
+    return $existed;
+}
+
+sub addFilter
+{
+    my ($self) = @_;
+    my $param = $self->getFilterParam();
+
+    # 新增服務
+    
+}
+
+sub getFilterRow
+{
+    my ($self) = @_;    
+
+    my $row;
+    
+    my $param = $self->getFilterParam();
+
+    return $row
+}
+
+sub getFilterParam
+{
+    my ($self, $port) = @_;
+
+    return (
+        # 設定連接埠參數
+    );
+}
+
+
 1;
