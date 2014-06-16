f022ac1ba7c1256fe652244892518f68d4536448
Pulipuli Chen <pulipuli.chen@gmail.com>
2014-06-16 17:08:09 +0800

    0616 這種就是這樣啦

--- dlllciasrouter/src/EBox/dlllciasrouter.pm
+++ dlllciasrouter/src/EBox/dlllciasrouter.pm
@@ -156,7 +156,7 @@ sub _setConf
 
         $self->writeConfFile(
             '/etc/pound/error.html',
-            "pound/error.html.mas",
+            "dlllciasrouter/error.html.mas",
             \@errorPageParams,
             { uid => '0', gid => '0', mode => '740' }
         );
@@ -301,7 +301,7 @@ sub _setConf
 
     $self->writeConfFile(
         $CONFFILE,
-        "pound/pound.cfg.mas",
+        "dlllciasrouter/pound.cfg.mas",
         \@servicesParams,
         { uid => '0', gid => '0', mode => '644' }
     );
@@ -310,14 +310,14 @@ sub _setConf
 
     $self->writeConfFile(
         '/etc/apache2/ports.conf',
-        "pound/ports.conf.mas",
+        "dlllciasrouter/ports.conf.mas",
         \@nullParams,
         { uid => '0', gid => '0', mode => '644' }
     );
 
     $self->writeConfFile(
         '/etc/default/pound',
-        "pound/default-pound.mas",
+        "dlllciasrouter/default-pound.mas",
         \@nullParams,
         { uid => '0', gid => '0', mode => '740' }
     );
@@ -329,7 +329,7 @@ sub _setConf
     $self->writeConfFile(
         '/etc/pound/vmid-config.php',
         #'/var/www/vmid-config.php',
-        "pound/vmid-config.php.mas",
+        "dlllciasrouter/vmid-config.php.mas",
         \@vmParams,
         { uid => '0', gid => '0', mode => '770' }
     );
