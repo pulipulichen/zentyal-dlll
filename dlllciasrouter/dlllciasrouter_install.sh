cf3b7a5533fa0c1ab31a6673dfc6dff4bb8fbd71
Pulipuli Chen <pulipuli.chen@gmail.com>
2014-06-16 23:06:31 +0800

    0616 設好安裝檔了，要準備go了嗎？

--- dlllciasrouter/dlllciasrouter_install.sh
+++ dlllciasrouter/dlllciasrouter_install.sh
@@ -1,6 +1,9 @@
+# https://github.com/pulipulichen/zentyal-dlll/raw/master/dlllciasrouter/dlllciasrouter_install.sh
+# http://goo.gl/P3qFIS
+
 cd ~
 sudo apt-get update
 sudo apt-get install -y pound
 
-wget -N https://github.com/pulipulichen/zentyal-dlll/raw/master/dlllciasrouter/debs-ppa/zentyal-pound_3.0_all.deb
+wget -N https://github.com/pulipulichen/zentyal-dlll/raw/master/dlllciasrouter/debs-ppa/zentyal-dlllciasrouter_3.0_all.deb
 sudo dpkg -i zentyal-dlllciasrouter_3.0_all.deb
