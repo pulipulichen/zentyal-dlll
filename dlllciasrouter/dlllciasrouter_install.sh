0b143bf318aed496f5f9e969ac6cabdc8105517d
Pulipuli Chen <pulipuli.chen@gmail.com>
2014-06-24 20:28:54 +0800

    Update dlllciasrouter_install.sh

--- dlllciasrouter/dlllciasrouter_install.sh
+++ dlllciasrouter/dlllciasrouter_install.sh
@@ -1,14 +1,1 @@
-# https://github.com/pulipulichen/zentyal-dlll/raw/master/dlllciasrouter/dlllciasrouter_install.sh
-
-# wget http://goo.gl/P3qFIS -O dlllciasrouter_install.sh
-# chmod 777 dlllciasrouter_install.sh
-# ./dlllciasrouter_install.sh
-
-# wget http://goo.gl/P3qFIS -O d.sh;chmod +x d.sh;./d.sh
-
-cd ~
-sudo apt-get update
-sudo apt-get install -y pound
-
-wget -N https://github.com/pulipulichen/zentyal-dlll/raw/master/dlllciasrouter/debs-ppa/zentyal-dlllciasrouter_3.0_all.deb
-sudo dpkg -i zentyal-dlllciasrouter_3.0_all.deb
+https://github.com/pulipulichen/zentyal-dlll/raw/3.0/dlllciasrouter/debs-ppa/zentyal-dlllciasrouter_3.0_all.deb
