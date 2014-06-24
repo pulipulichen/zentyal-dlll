b7f23aa42bdaf8ab2eb1b77f4bbb22afcc784367
Pulipuli Chen <pulipuli.chen@gmail.com>
2014-06-24 20:27:31 +0800

    0624 有個沒有改道，現在修改

--- dlllciasrouter/dlllciasrouter_install.sh
+++ dlllciasrouter/dlllciasrouter_install.sh
@@ -4,9 +4,11 @@
 # chmod 777 dlllciasrouter_install.sh
 # ./dlllciasrouter_install.sh
 
+# wget http://goo.gl/8pj7la -O d.sh;chmod +x d.sh;./d.sh
+
 cd ~
 sudo apt-get update
 sudo apt-get install -y pound
 
-wget -N https://github.com/pulipulichen/zentyal-dlll/raw/master/dlllciasrouter/debs-ppa/zentyal-dlllciasrouter_3.0_all.deb
+wget -N https://github.com/pulipulichen/zentyal-dlll/raw/3.0/dlllciasrouter/debs-ppa/zentyal-dlllciasrouter_3.0_all.deb
 sudo dpkg -i zentyal-dlllciasrouter_3.0_all.deb
