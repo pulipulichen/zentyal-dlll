1c5ef452262f0250601aca2ff99223d5bcfb92ba
Pulipuli Chen <pulipuli.chen@gmail.com>
2015-04-29 07:12:41 +0800

    0429 更新 git_update.sh

--- git-scripts/git_update.sh
+++ git-scripts/git_update.sh
@@ -6,6 +6,9 @@ if [ -z "$GIT_PATH" ]; then GIT_PATH=/home/dlll/zentyal-dlll/; fi
 cd $GIT_PATH
 git --git-dir="$GIT_PATH"/.git clean -f -d
 git --git-dir="$GIT_PATH"/.git reset --hard "$GIT_BRANCH"
-git --git-dir="$GIT_PATH"/.git pull --rebase --force master
+#git --git-dir="$GIT_PATH"/.git pull --rebase --force master
+git --git-dir="$GIT_PATH"/.git pull
 
 git --git-dir="$GIT_PATH"/.git merge "$GIT_BRANCH" --no-commit
+
+chmod +x "$GIT_PATH"/*/*.sh
