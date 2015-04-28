1b4df4218028d6bfb686b71704b754302e174c53
Pulipuli Chen <pulipuli.chen@gmail.com>
2015-04-29 07:09:44 +0800

    0429 調整 git_update.sh

--- git-scripts/git_update.sh
+++ git-scripts/git_update.sh
@@ -1,11 +1,11 @@
-if [ -z "$GIT_BRANCH" ]; then GIT_BRANCH=kals/master; fi
-if [ -z "$GIT_PATH" ]; then GIT_PATH=/var/www/moodle/kals; fi
+if [ -z "$GIT_BRANCH" ]; then GIT_BRANCH=origin/master; fi
+if [ -z "$GIT_PATH" ]; then GIT_PATH=/home/dlll/zentyal-dlll/; fi
 
 #echo $GIT_PATH
 
 cd $GIT_PATH
 git --git-dir="$GIT_PATH"/.git clean -f -d
 git --git-dir="$GIT_PATH"/.git reset --hard "$GIT_BRANCH"
-git --git-dir="$GIT_PATH"/.git pull --rebase --force kals
+git --git-dir="$GIT_PATH"/.git pull --rebase --force master
 
 git --git-dir="$GIT_PATH"/.git merge "$GIT_BRANCH" --no-commit
