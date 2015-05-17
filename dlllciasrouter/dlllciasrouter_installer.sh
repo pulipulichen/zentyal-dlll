d4cdbfced4e34140236374571850751f9c022600
Pulipuli Chen <pulipuli.chen@gmail.com>
2015-05-18 07:15:47 +0800

    0518 再開始

--- dlllciasrouter/dlllciasrouter_installer.sh
+++ dlllciasrouter/dlllciasrouter_installer.sh
@@ -2,6 +2,6 @@ sudo apt-get -y --force-yes update
 sudo apt-get -y --force-yes install zentyal-network zentyal-objects zentyal-firewall zentyal-dns zentyal-services zentyal-dhcp pound lighttpd
 
 cd /tmp
-wget https://raw.githubusercontent.com/pulipulichen/zentyal-dlll/master/dlllciasrouter/debs-ppa/zentyal-dlllciasrouter_3.4_all.deb
+wget https://raw.githubusercontent.com/pulipulichen/zentyal-dlll/master/dlllciasrouter/debs-ppa/zentyal-dlllciasrouter_3.4_all.deb -O zentyal-dlllciasrouter_3.4_all.deb
 sudo dpkg -i zentyal-dlllciasrouter_3.4_all.deb
 sudo /etc/init.d/zentyal dlllciasrouter restart
