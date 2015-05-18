3e50a8068cdf695b6cb0695ecc1ba9b3c5fd16e9
Pulipuli Chen <pulipuli.chen@gmail.com>
2015-05-19 05:39:23 +0800

    0519 更新安裝腳本

--- dlllciasrouter/dlllciasrouter_installer.sh
+++ dlllciasrouter/dlllciasrouter_installer.sh
@@ -38,8 +38,6 @@ else
     echo "All modules enabled."
 fi
 
-exit
-
 cd /tmp
 wget https://raw.githubusercontent.com/pulipulichen/zentyal-dlll/master/dlllciasrouter/debs-ppa/zentyal-dlllciasrouter_3.4_all.deb -O zentyal-dlllciasrouter_3.4_all.deb
 sudo dpkg -i zentyal-dlllciasrouter_3.4_all.deb
