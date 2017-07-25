6b0a47f29b487ed805389be668ea2794e9cc89aa
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-07-25 23:41:49 +0800

    0725 準備作git-init.sh的動作 測試

--- dlllciasrouter/src/EBox/dlllciasrouter.pm
+++ dlllciasrouter/src/EBox/dlllciasrouter.pm
@@ -103,7 +103,7 @@ sub menu
                                       'text' => __('Settings')));
 
     $folder->add(new EBox::Menu::Item('url' => 'dlllciasrouter/Composite/StorageServerComposite',
-                                      'text' => __('Distributed Storage AAA')));
+                                      'text' => __('Distributed Storage')));
 
     $folder->add(new EBox::Menu::Item('url' => 'dlllciasrouter/Composite/VEServerComposite',
                                       'text' => __('Virtual Environment')));
