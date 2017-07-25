1470bc740e6dffaca5b2bdf493fc9bd92a7d83d5
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-07-25 23:31:22 +0800

    0725 準備作git-init.sh的動作

--- dlllciasrouter/git-init.sh
+++ dlllciasrouter/git-init.sh
@@ -1,6 +1,6 @@
 sudo apt-get -y install git
 cd ~
 git clone https://github.com/pulipulichen/zentyal-dlll.git
-chmod +x ~/zentyal-dlll/git-scripts/*.sh
+chmod +x ~/zentyal-dlll/*/*.sh
 cd ~/zentyal-dlll/dlllciasrouter/
 complie.sh
