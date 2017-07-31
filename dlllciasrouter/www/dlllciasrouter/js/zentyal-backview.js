c4e98b264e6a6195f4ca7fa0fa0ceaa73b141d88
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-07-31 22:40:04 +0800

    0731 Pound全部完成，而且也加強了回到上一頁的功能

--- dlllciasrouter/www/dlllciasrouter/js/zentyal-backview.js
+++ dlllciasrouter/www/dlllciasrouter/js/zentyal-backview.js
@@ -1,52 +1,79 @@
 $(function () {
-    setTimeout(function () {
-        var getQueryVariable = function (variable) {
-            var query = window.location.search.substring(1);
-            var vars = query.split("&");
-            for (var i = 0; i < vars.length; i++) {
-                var pair = vars[i].split("=");
-                if (pair[0] === variable) {
-                    return pair[1];
-                }
-            }
-            //alert('Query Variable ' + variable + ' not found');
-            return undefined;
-        };
-
-        var _backview = getQueryVariable("backview");
-        var _backview_title = getQueryVariable("backview_title");
-        var _backview_hash = getQueryVariable("backview_hash");
-        //console.log(_backview);
-        //alert(_backview);
-
-        if (_backview !== undefined) {
-            var _h1 = $("h1:first");
-            //alert(_h1.length);
-            var _domainName = _h1.text().trim().split(" ")[0];
-            //alert(_domainName.split(".").length);
-            var _a;
-            if (_domainName.split(".").length > 1) {
-                var _uri = _backview + "?filter=" + _domainName;
-                if (_backview_hash !== undefined) {
-                    _uri = _uri + "#" + _backview_hash;
-                }
-                _a = $('<a href="' + _uri + '">' + _h1.html() + '</a>');
-                _h1.html(_a);
-            }
-            else if (_backview_title !== undefined) {
-                if (_backview_hash !== undefined) {
-                    _backview = _backview + "#" + _backview_hash;
-                }
-                _a = $('<span><a href="' + _backview + '">' + _backview_title + '</a>  &gt;</span>');
-                _h1.prepend(_a);
-            }
-            else {
-                if (_backview_hash !== undefined) {
-                    _backview = _backview + "#" + _backview_hash;
-                }
-                _a = $('<a href="' + _backview + '">' + _h1.html() + '</a>');
-                _h1.html(_a);
+    var getQueryVariable = function (variable) {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split("=");
+            if (pair[0] === variable) {
+                return pair[1];
             }
         }
-    }, 0);
+        //alert('Query Variable ' + variable + ' not found');
+        return undefined;
+    };  // var getQueryVariable = function (variable) {
+    
+    var _setup_h1 = function (_h1, _a) {
+        if (_h1.find(".title_link_sep").length === 0) {
+            _h1.html(_a);
+        }
+        else {
+            var _h1_html_array = _h1.html().trim().split("\n");
+            _h1_html_array.pop();
+            _h1.html(_h1_html_array.join("\n")).append(_a);
+        }
+    };
+        
+    // --------------------------
+
+    var _backview = getQueryVariable("backview");
+    var _backview_title = getQueryVariable("backview_title");
+    var _backview_hash = getQueryVariable("backview_hash");
+    //console.log(_backview);
+    //alert(_backview);
+
+    if (_backview !== undefined) {
+        var _h1 = $("h1:first");
+        //alert(_h1.length);
+        var _domainName;
+        if (_h1.find(".title_link_sep").length === 0) {
+            _domainName = _h1.text().trim().split(" ")[0];
+        }
+        else {
+            _domainName = _h1.text().trim().split("\n")[2].trim().split(" ")[0];
+        }
+
+        //alert(_domainName.split(".").length);
+        var _a;
+        if (_domainName.split(".").length > 1) {
+            var _uri = _backview + "?filter=" + _domainName;
+            if (_backview_hash !== undefined) {
+                _uri = _uri + "#" + _backview_hash;
+            }
+            
+            if (_h1.find(".title_link_sep").length === 0) {
+                _a = $('<a href="' + _uri + '">' + _h1.html() + '</a>');
+                _h1.html(_a);
+                //_setup_h1(_h1, _a);
+            }
+            else {
+                //alert(_uri);
+                _h1.find("a:eq(1)").attr("href", _uri);
+            }
+        }
+        else if (_backview_title !== undefined) {
+            if (_backview_hash !== undefined) {
+                _backview = _backview + "#" + _backview_hash;
+            }
+            _a = $('<span><a href="' + _backview + '">' + _backview_title + '</a>  &gt;</span>');
+            _h1.prepend(_a);
+        }
+        else {
+            if (_backview_hash !== undefined) {
+                _backview = _backview + "#" + _backview_hash;
+            }
+            _a = $('<a href="' + _backview + '">' + _h1.html() + '</a>');
+            _setup_h1(_h1, _a);
+        }
+    }
 });
+
