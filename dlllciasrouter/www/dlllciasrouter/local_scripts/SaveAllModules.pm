6949d48c9e61c0f6d1c73bd342c749628a857523
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-10-28 00:45:28 +0800

    1028 可以lock嗎？不可以，放棄吧2

--- dlllciasrouter/www/dlllciasrouter/local_scripts/SaveAllModules.pm
+++ dlllciasrouter/www/dlllciasrouter/local_scripts/SaveAllModules.pm
@@ -6,33 +6,10 @@ use EBox;
 use EBox::Config;
 use EBox::Global;
 
-my $file = "/tmp/SaveAllModules.pm.lock";
-
-if (-f $file)
-{ 
-  print "SaveAllModules.pm is locked.\n";
-  exit;
-}
-
-# Use the open() function to create the file.
-unless(open FILE, '>'.$file) {
-    # Die with error message 
-    # if we can't open it.
-    die "\nUnable to create $file\n";
-}
-
-# Write some text to the file.
-print FILE "SaveAllModules.pm is locked.";
-
-# close the file.
-close FILE;
-
 EBox::init();
 
 print "Saving all modules\n";
 my $global = EBox::Global->getInstance();
 $global->saveAllModules();
-
-system('sudo rm -rf /tmp/SaveAllModules.pm.lock');
 
 print "Modules saved\n";
