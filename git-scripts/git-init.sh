7e7b09a09659e3d8f884b859b0a432b85dfbe10e
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-10-27 23:16:43 +0800

    1027 多的git-scripts?

--- git-scripts/git-init.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get -y install git
-cd ~
-git clone https://github.com/pulipulichen/zentyal-dlll.git
-chmod +x ~/zentyal-dlll/git-scripts/*.sh
-cd ~/zentyal-dlll/dlllciasrouter/
-complie.sh
