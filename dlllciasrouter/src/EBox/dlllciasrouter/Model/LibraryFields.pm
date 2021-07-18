0119032e54057519dcbc5c3a1f59accf4269a247
Pulipuli Chen <blog@pulipuli.info>
2021-07-19 01:30:23 +0800

    0719 test domain name: 設定DNS

--- dlllciasrouter/src/EBox/dlllciasrouter/Model/LibraryFields.pm
+++ dlllciasrouter/src/EBox/dlllciasrouter/Model/LibraryFields.pm
@@ -81,7 +81,7 @@ sub createFieldDomainName
         #'unique' => 1,
         'hiddenOnSetter' => 0,
         'hiddenOnViewer' => 1,
-        'help' => __('<a href="https://github.com/pulipulichen/zentyal-dlll/blob/master/guide/5-1-domain-name-rule.md" target="_blank">Domain name rule</a>: exp-example-2018.dlll.nccu.edu.tw') 
+        'help' => '<a href="https://github.com/pulipulichen/zentyal-dlll/blob/master/guide/5-1-domain-name-rule.md" target="_blank">Domain name rule</a>: exp-example-2018.dlll.nccu.edu.tw'
           . $self->setFieldCacheScript('domainName'),
         'HTMLSetter' => '/ajax/setter/textFullWidthSetter.mas',
     );
@@ -98,7 +98,7 @@ sub createFieldDomainNameUnique
         'unique' => 1,
         'hiddenOnSetter' => 0,
         'hiddenOnViewer' => 1,
-        'help' => __('<a href="https://github.com/pulipulichen/zentyal-dlll/blob/master/guide/5-1-domain-name-rule.md" target="_blank">Domain name rule</a>: exp-example-2018.dlll.nccu.edu.tw')
+        'help' => '<a href="https://github.com/pulipulichen/zentyal-dlll/blob/master/guide/5-1-domain-name-rule.md" target="_blank">Domain name rule</a>: exp-example-2018.dlll.nccu.edu.tw'
            . $self->setFieldCacheScript('domainName'),
         'HTMLSetter' => '/ajax/setter/textFullWidthSetter.mas',
     );
