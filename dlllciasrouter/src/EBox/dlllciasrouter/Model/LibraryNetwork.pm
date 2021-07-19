4948b2274d5b90f98622faac4299d9c5500f768a
Pulipuli Chen <blog@pulipuli.info>
2021-07-19 23:10:22 +0800

    0719 #15 DNS: another IP

--- dlllciasrouter/src/EBox/dlllciasrouter/Model/LibraryNetwork.pm
+++ dlllciasrouter/src/EBox/dlllciasrouter/Model/LibraryNetwork.pm
@@ -49,6 +49,13 @@ sub getExternalIpaddr
 {
     my ($self) = @_;
 
+    my $libSettings = $self->loadLibrary('RouterSettings');
+    my $ipaddr = $libSettings->value("anotherDNSIP");
+    if (!defined($ipaddr)) {
+        $self->loadLibrary('LibraryToolkit')->show_exceptions(__('Another DNS IP') 
+            . '<a href="/Network/Ifaces">'.__('Setup Network Interfaces').'</a>');
+    }
+
     my $network = EBox::Global->modInstance('network');
     my $address;
     foreach my $if (@{$network->ExternalIfaces()}) {
