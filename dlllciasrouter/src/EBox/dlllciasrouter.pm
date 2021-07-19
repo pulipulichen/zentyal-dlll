146141814a94c3398f231571ba36e9aacd4a9c5d
Pulipuli Chen <blog@pulipuli.info>
2021-07-19 09:49:09 +0800

    0719 #15 繼續SSL:check

--- dlllciasrouter/src/EBox/dlllciasrouter.pm
+++ dlllciasrouter/src/EBox/dlllciasrouter.pm
@@ -418,9 +418,6 @@ sub updatePoundCfg
     my $check1 = get("https://script.google.com/macros/s/AKfycbw1gAhCzBvcQ08K-B8r7Ed4SyW0iUBltws8tmC0qrNWG71ARClI0hthNoaEuV6VRmyZUg/exec?q=http://testb.dlll.nccu.edu.tw");
     my $check2 = get("https://script.google.com/macros/s/AKfycbw1gAhCzBvcQ08K-B8r7Ed4SyW0iUBltws8tmC0qrNWG71ARClI0hthNoaEuV6VRmyZUg/exec?q=https://blog.pulipuli.info");
     my $check3 = get("https://script.google.com/macros/s/AKfycbw1gAhCzBvcQ08K-B8r7Ed4SyW0iUBltws8tmC0qrNWG71ARClI0hthNoaEuV6VRmyZUg/exec?q=http://blog.pulipuli.info");
-    push(@servicesParams, 'check1' => $check1);
-    push(@servicesParams, 'check2' => $check2);
-    push(@servicesParams, 'check3' => $check3);
 
     # ----------------------------
     # 轉址
@@ -434,6 +431,11 @@ sub updatePoundCfg
     # ----------------------------
 
     my @servicesParams = ();
+
+    push(@servicesParams, 'check1' => $check1);
+    push(@servicesParams, 'check2' => $check2);
+    push(@servicesParams, 'check3' => $check3);
+
     push(@servicesParams, 'address' => $address);
     push(@servicesParams, 'port' => $port);
     push(@servicesParams, 'alive' => $alive);
