be7f6553c35f64e653c3260abb67e5a20683e1ec
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-07-02 09:14:55 +0800

    Sat Jul  2 09:14:55 AM CST 2022

--- dlllciasrouter/src/EBox/dlllciasrouter/Model/LibraryServiceXRDP.pm
+++ dlllciasrouter/src/EBox/dlllciasrouter/Model/LibraryServiceXRDP.pm
@@ -30,6 +30,40 @@ sub getLibrary
     return $self->parentModule()->model("LibraryToolkit");
 }
 
+##
+# 讀取指定的Model
+#
+# 我這邊稱之為Library，因為這些Model是作為Library使用，而不是作為Model顯示資料使用
+# @author 20140312 Pulipuli Chen
+sub loadLibrary
+{
+    my ($self, $library) = @_;
+    return $self->parentModule()->model($library);
+}
+
+# ------------------------------------------------
+
+# 20170303 Pulipuli Chen
+sub updateXRDPCfg
+{
+    my ($self) = @_;
+    my @servicesParams = ();
+
+    my $settings = $self->loadLibarary('RouterSettings');
+
+    my $xrdpPort = $settings->value('xrdpPort');
+    push(@servicesParams, 'xrdpPort' => $xrdpPort);
+
+    $self->parentModule()->writeConfFile(
+        '/etc/xrdp/xrdp.ini',
+        "dlllciasrouter/xrdp.ini.mas",
+        \@servicesParams,
+        { uid => '0', gid => '0', mode => '644' }
+    );
+
+    EBox::Sudo::root("/etc/init.d/xrdp restart");
+}   # sub updateXRDPCfg
+
 # -----------------------------------------------
 
 1;
