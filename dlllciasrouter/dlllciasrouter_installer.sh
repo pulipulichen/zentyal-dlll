43f5af912db7ca9305496a6723c073ebf07f23c3
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-07-06 14:13:11 +0800

    Wed Jul  6 02:13:11 PM CST 2022

--- dlllciasrouter/dlllciasrouter_installer.sh
+++ dlllciasrouter/dlllciasrouter_installer.sh
@@ -11,7 +11,7 @@ chmod +x ~/d.sh
 if [ -f ~/git-init.sh ] ; then
     # 已經下載的狀態下，我們只做程式碼的更新
     clear
-    bash ~/zentyal-dlll/dlllciasrouter/git_update_compile.sh
+    bash ~/zentyal-dlll/dlllciasrouter/git_update_compile_log.sh
     echo "===================================";
     echo "DLLL-CIAS Router is updated"
     echo "===================================";
