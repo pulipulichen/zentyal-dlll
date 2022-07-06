b5a0e9646c51c15e161d31e7ba4ab064250ed0b1
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-07-06 18:13:20 +0800

    Wed Jul  6 06:13:20 PM CST 2022

--- dlllciasrouter/stubs/certbot-pound.sh
+++ dlllciasrouter/stubs/certbot-pound.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 for domain in $RENEWED_DOMAINS; do 
-  cat "/etc/letsencrypt/live/${domain}/privkey.pem" "/etc/letsencrypt/live/${domain}/fullchain.pem" > "/etc/pound/cert/dlllciasrouter.pem"
+  if [ -d "/etc/letsencrypt/live/${domain}" ]
+  then
+    cat "/etc/letsencrypt/live/${domain}/privkey.pem" "/etc/letsencrypt/live/${domain}/fullchain.pem" > "/etc/pound/cert/${domain}.pem"
+  fi
 done
 /etc/init.d/pound restart
