eafc06a32e4c794ab98b6a03e0e4b0cf0306602a
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-07-11 16:25:09 +0800

    Mon Jul 11 04:25:09 PM CST 2022

--- dlllciasrouter/src/EBox/dlllciasrouter/Model/LibraryPoundBackend.pm
+++ dlllciasrouter/src/EBox/dlllciasrouter/Model/LibraryPoundBackend.pm
@@ -650,7 +650,7 @@ sub setRunCertbot
       { uid => '0', gid => '0', mode => '770' }
     );
 
-    EBox::Sudo::root($scriptPath);
+    #EBox::Sudo::root($scriptPath);
 }
 
 # -----------------------------------------------
